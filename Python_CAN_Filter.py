c9eed8cdc45ce1d0f862632cc4de5c5155af79a6
Matt Poser <21mep17@queensu.ca>
2025-03-22 15:43:46 -0400

    Update Python_CAN_Filter.py
    Added list of instructions for 3 basic CAN values to display on dash

--- Python_CAN_Filter.py
+++ Python_CAN_Filter.py
@@ -5,7 +5,58 @@ Created on Sat Mar  8 11:24:04 2025
 @author: m_epo
 """
 
+"""
+Instruction List, for operating on bits
+.dbc file has the different pieces of data listed as bits, with a byte length of 8 & a starting bit
+following our findings in the workshop, this corresponds to single byte array elements
 
+ie, a message, starting at bit 0 with byte length 8 is message.data[0] (0th element in the array)
+starting bit 32 would be the 4th byte, meaning message.data[3] (3rd index element in the array)
+
+
+For our dashboard, we need the following pieces of data (just to start, we can get more later):
+under each data piece, instructions & ID from .dbc file are listed
+
+Car battery Level
+~~~~~~~~~~~~~~~~~~~
+'VCU_Vehicle_Status_1' message
+
+ID = 1304
+start bit = 32 
+data type (to be interpreted as) = unsigned integer
+factor = 1   #multiply value by one
+offset = 0   #add 0 to the value
+
+The above is an example of a piece of data which needs no offset or factor applied to it,
+we can just pull it straight from the message.data byte array
+
+
+
+
+Car Wheelspeed
+~~~~~~~~~~~~~~~~~~~
+'VCU_Vehicle_Status_1' message
+*Byte order is listed as little Endian, I think python handles this automatically*
+ID = 1304
+start bit = 16 
+data type = unsigned integer
+factor = 1  
+offset = 0  
+
+Car RPM
+~~~~~~~~~~~~~~~~~~~
+'VCU_Inverter_Status_2'
+
+ID = 1303
+start bit = 0
+data type = signed integer  (-10000 to 10000 min & max values)
+factor = 1  
+offset = 0  
+
+
+
+
+"""
 
 
 #To run this, we'll need to install 'python-can' (I believe it's already installed)
